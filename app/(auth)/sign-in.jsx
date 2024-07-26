63fbc31e35ac67aa40614d19e211dfaaa58b4487
Ibrahim Yakubu <ibrahimy437@gmail.com>
2024-07-26 17:08:02 +0100

    add log in text and logo to sign-in page

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,12 +1,16 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants'
 
 const SignIn = () => {
   return (
     <SafeAreaView className='bg-primary h-full'>
       <ScrollView>
-        <View className='w-full justify-center min-h-[85vh] px-4 my-6'></View>
+        <View className='w-full justify-center h-full px-4 my-6'>
+          <Image source={images.logo} className='w-[115px] h-[35px]' resizeMode='contain' />
+          <Text className='text-2xl text-white text-semibold mt-10 font-psemibold'>Log in to Aora</Text>
+        </View>
       </ScrollView>
     </SafeAreaView>
   )
